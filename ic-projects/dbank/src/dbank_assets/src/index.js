f53ff2508698321083c6583731ad85602f1bd24a
techamit95ch <amit98ch@gmail.com>
2022-05-26 07:20:54 +0530

    feat: dfx compond balance

--- ic-projects/dbank/src/dbank_assets/src/index.js
+++ ic-projects/dbank/src/dbank_assets/src/index.js
@@ -1,19 +1,37 @@
 import { dbank } from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+window.addEventListener("load", async function () {
+  const currentAmmount = await dbank.getBalance();
+
+  document.getElementById("balance").innerText =
+    Math.round(currentAmmount * 100) / 100;
+});
+
+document.querySelector("form").addEventListener("submit", async function (e) {
   e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
+  const button = document.querySelector("button");
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
+  const withdrawl = document.getElementById("withdrawl").value;
+  const deposit = document.getElementById("deposit").value;
+  if (
+    withdrawl &&
+    withdrawl.length !== 0 &&
+    withdrawl.length !== NaN &&
+    withdrawl > 0
+  ) {
+    await dbank.withDrawl(Number(withdrawl));
+  }
+  if (
+    deposit &&
+    deposit.length !== 0 &&
+    deposit.length !== NaN &&
+    deposit > 0
+  ) {
+    await dbank.toUp(Number(deposit));
+  }
+  await dbank.compound();
+  document.getElementById("balance").innerText =
+    Math.round((await dbank.getBalance()) * 100) / 100;
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
 });
