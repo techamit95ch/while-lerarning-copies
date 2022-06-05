ab256a2ed2865191cde2809d6ec2c0e5c8254ca7
techamit95ch <amit98ch@gmail.com>
2022-06-05 21:51:03 +0530

    feat: basic errors

--- solana/rust-practices/hello/src/main.rs
+++ solana/rust-practices/hello/src/main.rs
@@ -1,167 +1,89 @@
-// create Enum For Color
-pub enum Color {
-    Red,
-    Green,
-    Blue,
-}
+use std::collections::HashMap;
 
-impl std::fmt::Debug for Color {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Red => write!(f, "Red"),
-            Self::Green => write!(f, "Green"),
-            Self::Blue => write!(f, "Blue"),
-        }
+// Options
+// None to indicate failure or lack of value, and
+//  Some(Value) , a tuple struct that wraps a value with Type T
+//  Option<T> , a type alias for Option<T>
+
+fn devide(devident: i32, devisor: i32) -> Option<i32> {
+    if devident % devisor != 0 {
+        None
+    } else {
+        Some(devident / devisor)
     }
 }
 
-pub enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-// structure for point
+// Error Enum
+//  ErrorKind, a type alias for enum
+//  Error, a tuple struct that wraps a value with Type T
+//  Result<T, E>, a type alias for Result<T, E>
+
 #[derive(Debug)]
-pub struct Point {
-    color: Color,
-    __x: i32,
-    __y: i32,
+enum Error {
+    DivisionByZero,
+    NegativeLogarithm,
+    NegativeSquareRoot,
+    CustomError,
 }
 
-impl std::ops::DerefMut for Point {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.color
-    }
-}
+// Result
+//  Ok(T) , a type alias for Result<T, E>
+//  Err(E) , a type alias for Result<T, E>
 
-impl std::ops::Deref for Point {
-    type Target = Color;
-
-    fn deref(&self) -> &Self::Target {
-        &self.color
-    }
-}
-
-impl Point {
-    /// Creates a new [`Point`].
-    pub fn new(__x: i32, __y: i32, color: Color) -> Self {
-        Self { __x, __y, color }
+fn sqrt(x: f64) -> Result<f64, Error> {
+    if x < 0.0 {
+        Err(Error::NegativeSquareRoot)
+    } else {
+        Ok(x.sqrt())
     }
 }
 
 fn main() {
-    // Implement Color
-    //
-    // Implement the `Color` trait for the `Point` struct below. `Color` trait
-    // requires `fmt::Display` be implemented for `Point`.
-    //
-    // The output should look like:
-    // ```text
-    // Point(x: 2, y: 3, color: Color(Red, Green))
-    // ```
-    //
-    // You can refer to the `colors` module for the `Color` enum.
-    //
-    // Your `main` function should call `point` and pass in the correct parameters.
-    //
-    // Execute `rustlings hint colors` to get more information about the Color enum.
-    //
-    // You can search for the `colors` module to find the documentation for the Color enum.
-    //
-    // If you get stuck, you can find a solution in the Rust Book:
-    // https://doc.rust-lang.org/book/second-edition/ch03-02-defining-structs.html
-    //
-    // You can also ask for help on this exercise on the discussion forums.
+    // Vector
+    let mut vec: Vec<i64> = vec![1, 2, 3, 4, 5];
+    vec.len();
+    vec[0];
+    vec.push(6);
+    vec.remove(0);
+    println!("{:?}", vec);
 
-    let point: Point = Point {
-        __x: 2,
-        __y: 3,
-        color: Color::Red,
-    };
-
-    match point.color {
-        Color::Red => println!("Red"),
-        Color::Green => println!("Green"),
-        Color::Blue => println!("Blue"),
+    // HashMap
+    let mut map = HashMap::new();
+    map.insert(0, "one".to_string());
+    map.insert(1, "two".to_string());
+    map.insert(2, "three".to_string());
+    map.insert(3, "four".to_string());
+    println!("{:?}", map);
+    match map.get(&0) {
+        Some(value) => println!("{}", value),
+        None => println!("None"),
     }
+    map.remove(&0);
+    println!("{:?}", map);
 
-    println!(r#"Point({:?})"#, point);
-    let _point_red: Point = Point::new(2, 3, Color::Red);
-    let point_green: Point = Point::new(2, 3, Color::Green);
+    // Option
+    let devide1: Option<i32> = devide(10, 2);
+    // let devide2: Option<i32> = devide(2, 3);
 
-    match point_green {
-        Point {
-            color: Color::Red, ..
-        } => println!("Red Point"),
-        Point {
-            color: Color::Green,
-            ..
-        } => println!("Green Point"),
-        Point {
-            color: Color::Blue, ..
-        } => println!("Blue Point"),
+    // unwraping a Some
+    println!("{:?} unwraps => {}", devide1, devide1.unwrap());
+    // unwraping a None will throw a `panic!`
+    // println!("{:?} unwraps => {}", devide2, devide2.unwrap());
+
+    // square root
+    let sqrt1: Result<f64, Error> = sqrt(9.0);
+    let sqrt2: Result<f64, Error> = sqrt(-9.0);
+    if sqrt1.is_ok() {
+        println!("{:?}", sqrt1.unwrap());
     }
-
-    let mut _a = MyEnum::A;
-    let mut _b = MyEnum::B(100);
-    let mut _c = MyEnum::C(C { __x: 1, __y: 2 });
-
-    println!("{:?}", (_a, _b, _c));
-}
-
-// Enums
-
-#[derive(Debug)]
-struct C {
-    __x: i32,
-    __y: i32,
-}
-
-enum MyEnum {
-    A,
-    B(i32),
-    C(C),
-}
-
-impl std::fmt::Debug for MyEnum {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::A => write!(f, "A"),
-            Self::B(arg0) => f.debug_tuple("B").field(arg0).finish(),
-            Self::C(C { __x, __y }) => f.debug_struct("C").field("x", __x).field("y", __y).finish(),
-        }
+    if sqrt2.is_err() {
+        println!("{:?}", sqrt2.unwrap_err());
     }
+    // unwrap_or
+    let sqrt3: f64 = sqrt(9.0).unwrap_or(0.0);
+    println!("{:?}", sqrt3);
+    // unwrap_or_else
+    let sqrt4: Result<f64, Error> = sqrt(-9.0);
+    let errExpect = sqrt4.expect("We crashed");
+    println!("{:?}", errExpect);
 }
-pub struct ColorStruct {
-    r: u8,
-    g: u8,
-    b: u8,
-}
-
-impl std::ops::DerefMut for ColorStruct {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.g
-    }
-}
-
-impl std::ops::Deref for ColorStruct {
-    type Target = u8;
-
-    fn deref(&self) -> &Self::Target {
-        &self.r
-    }
-}
-
-impl ColorStruct {
-    pub fn new(r: u8, g: u8, b: u8) -> ColorStruct {
-        ColorStruct { r, g, b }
-    }
-
-    pub fn b_mut(&mut self) -> &mut u8 {
-        &mut self.b
-    }
-}
-
-// Language: rust
-// Path: src/main.rs
